90a5dc8809fac98b4154e1fe977fbd258514f6eb
kevinmbt <kperilla@utexas.edu>
2018-09-23 15:23:55 -0500

    ya know this may work this may not you never really know

--- hw-template/hw2/q3/Monkey.java
+++ hw-template/hw2/q3/Monkey.java
@@ -1,30 +1,25 @@
 package q3;
 
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.Arrays;
+import java.util.concurrent.locks.*;
 
 public class Monkey {
     private static class ClimbingPrivilege {
-        private ReentrantLock lock;
-        public int direction = -1;
+        final private ReentrantLock lock = new ReentrantLock();
+        final Condition noOpposingMonkeys = lock.newCondition();
 
-        public ClimbingPrivilege() {
-            lock = new ReentrantLock();
-        }
+        public int direction = -1;
 
         // Only called by kong
         public void lock() {
             lock.lock();
         }
 
-        public boolean tryLock(int direction) {
+        public boolean tryLock(int direction) throws InterruptedException {
             boolean lockFree = lock.tryLock();
             if (lockFree) {
-                if (noOpposingMonkeys(direction)) {
-                    this.direction = direction;
-                } else {
-                    lock.unlock();
-                    lockFree = false;
-                }
+                noOpposingMonkeys.await();
+                this.direction = direction;
             }
             return lockFree;
         }
@@ -37,70 +32,166 @@ public class Monkey {
         public int getHoldCount() {
             return lock.getHoldCount();
         }
-
-        private boolean noOpposingMonkeys(int direction) {
-            return ((Monkey.re1.direction == direction
-                    || Monkey.re1.direction == -1)
-                    && (Monkey.re2.direction == direction
-                    || Monkey.re2.direction == -1)
-                    && (Monkey.re3.direction == direction
-                    || Monkey.re3.direction == -1));
-        }
     }
 
-    private static ClimbingPrivilege re1 = new ClimbingPrivilege();
-    private static ClimbingPrivilege re2 = new ClimbingPrivilege();
-    private static ClimbingPrivilege re3 = new ClimbingPrivilege();
+//    private static ClimbingPrivilege[] availableClimbSpots;
 
-    private ThreadLocal<ClimbingPrivilege> myLock;
+//    private ThreadLocal<ClimbingPrivilege> myLock;
 
     private static boolean kongOnRope = false;
+    private static int numMonkeysOnRope = 0;
+    private static int directions[] = {-2, -2, -2};
+    private ThreadLocal<Integer> monkeyIndex;
+
+    private static final ReentrantLock ropeLock = new ReentrantLock();
+    private static final Condition noMonkeys = ropeLock.newCondition();
+    private static final Condition noOpposingMonkeys = ropeLock.newCondition();
+    private static final Condition notFull = ropeLock.newCondition();
+    private static final Condition noKong = ropeLock.newCondition();
+
+    static int oppWaits = 0;
+    static int fullWaits = 0;
 
     public Monkey() {
-        myLock = ThreadLocal.withInitial(() -> new ClimbingPrivilege());
+        monkeyIndex = ThreadLocal.withInitial(() -> -1);
+    }
+
+    private boolean AreOpposingMonkeys(int direction) {
+        boolean areOpposers = false;
+        for (int dir : directions) {
+            if (!(dir == direction || dir == -2)) {
+                areOpposers = true;
+            }
+        }
+        return areOpposers;
     }
 
     public void ClimbRope(int direction) throws InterruptedException {
-        if (direction == -1) {
-            try {
-                re1.lock();
-                re2.lock();
-                re3.lock();
-                myLock.set(re1);
-            } catch (Exception e) {
-                throw new InterruptedException();
-            }
-            kongOnRope = true;
-        }
-        else {
-            while (kongOnRope) {}
-            boolean available = false;
-            try {
-                while (!available) {
-                    if (re1.tryLock(direction)) {
-                        myLock.set(re1);
-                        available = true;
-                    } else if (re2.tryLock(direction)) {
-                        myLock.set(re2);
-                        available = true;
-                    } else if (re3.tryLock(direction)) {
-                        myLock.set(re3);
-                        available = true;
-                    }
-                    // Without this, my computer can't handle it lol
-                    Thread.sleep(5);
+//        System.out.println("direction: " + direction);
+        ropeLock.lock();
+//        System.out.println("Entering, monkeys: " + numMonkeysOnRope);
+        String reason = "new";
+        try {
+            if (direction == -1) {
+                kongOnRope = true;
+//            ropeLock.lock();
+                while (numMonkeysOnRope > 0) {
+                    System.out.println("KONG IS WAITING");
+                    noMonkeys.await();
+                    System.out.println("KONG IS DONE WAITING");
                 }
-            } catch (Exception e) {
-                throw new InterruptedException();
+            } else {
+                while (kongOnRope) {
+//                    System.out.println("Awaiting kong");
+//                    System.out.println("Awaiting kong: " + Thread.currentThread().getId());
+                    noKong.await();
+                    reason = "Kong";
+//                    System.out.println("kong left, numMonkeys: " + numMonkeysOnRope);
+                }
+                while (AreOpposingMonkeys(direction)) {
+//                    System.out.println("Awaiting opposers: " + Thread.currentThread().getId());
+                    noOpposingMonkeys.await();
+                    reason = "Opposing";
+                    System.out.println("opposers may have left. numMonkeys: " + numMonkeysOnRope);
+                }
+                while (numMonkeysOnRope == 3) {
+//                    System.out.println("Awaiting full: " + Thread.currentThread().getId());
+                    notFull.await();
+                    reason = "full";
+//                    System.out.println("full may not be true" + Thread.currentThread().getId());
+                }
+
             }
+            int position = 3;  // Error will occur if not found
+            for (int i = 0; i < 3; i++) {
+                if (directions[i] == -2) {
+                    position = i;
+                    break;
+                }
+            }
+            if (position == 3) {
+                System.out.println("POSITION SHOULD NOT BE 3. numMonkeys: " + numMonkeysOnRope + " reason: " + reason);
+                System.out.println(Arrays.toString(directions));
+                Thread.sleep(999999999);
+            }
+            if (direction == -1) {
+                position = 0;
+                System.out.println("KONG GETTING ON");
+            }
+            try {
+                int orig = directions[position];
+                directions[position] = direction;
+                System.out.println("Setting direction. position: " + position + " dir: " + directions[position] + " orig: " + orig + "reason: " + reason + " " + Arrays.toString(directions));
+//                System.out.println(Arrays.toString(directions));
+            } catch (ArrayIndexOutOfBoundsException e) {
+                e.printStackTrace();
+//                System.out.println(Arrays.toString(directions));
+            }
+//            System.out.println("num Monkeys: " + numMonkeysOnRope);
+//            System.out.println(Arrays.toString(directions));
+            // TODO: Either direction isn't updating right, or monkeys are jumping the gun
+            monkeyIndex.set(position);
+//            System.out.println("Setting index");
+//            System.out.println(position);
+            numMonkeysOnRope++;
+            System.out.println("Entered, monkeys: " + numMonkeysOnRope);
+        } finally {
+            ropeLock.unlock();
         }
     }
 
     public void LeaveRope() {
-        if (myLock.get().direction == -1) {
-            kongOnRope = false;
+//        System.out.println("Inside LeaveRope");
+        ropeLock.lock();
+//        System.out.println("Leaving, monkeys: " + numMonkeysOnRope);
+
+        try {
+//            System.out.println(monkeyIndex.get());
+            if (monkeyIndex.get() >= 3 || monkeyIndex.get() < 0) {
+                System.out.println("BAD INDEX: " + monkeyIndex.get());
+            }
+            int myDirection = directions[monkeyIndex.get()];
+            directions[monkeyIndex.get()] = -2;
+
+            numMonkeysOnRope--;
+            if (numMonkeysOnRope < 0) {
+                System.out.println("num under 0");
+            }
+//            System.out.println("Num monkeys: " + numMonkeysOnRope);
+
+
+            if (myDirection == -1) {
+                System.out.println("Kong getting off, index: " + monkeyIndex.get() + " " + Arrays.toString(directions));
+//                directions[monkeyIndex.get()] = -2;
+                kongOnRope = false;
+                System.out.println("SETTING KONG FALSE");
+                noKong.signalAll();
+            }
+//            if (ropeLock.hasWaiters(noOpposingMonkeys)) {
+//                noOpposingMonkeys.signal();
+//            }
+
+            notFull.signal();
+
+             if (numMonkeysOnRope == 0) {
+                System.out.println("SIGNALLING NO MONKEYS");
+                noMonkeys.signal();
+                noOpposingMonkeys.signal();
+            }
+            monkeyIndex.set(-1);
+
+
+//            fullWaits--;
+//            System.out.println("Signalling the next monkey to go");
+//            oppWaits--;
+
+
+        } finally {
+//            System.out.println("Left, monkeys: " + numMonkeysOnRope);
+//            System.out.println("oppwaits: " + oppWaits);
+//            System.out.println("fullwaits: " + fullWaits);
+            ropeLock.unlock();
         }
-        myLock.get().unlock();
     }
 
     /**
@@ -113,9 +204,10 @@ public class Monkey {
      * case 2: when Kong is on the rope, this value should be 1
      */
     public int getNumMonkeysOnRope() {
-        if (kongOnRope) {
-            return 1;
-        }
-        return re1.getHoldCount() + re2.getHoldCount() + re3.getHoldCount();
+//        if (kongOnRope) {
+//            return 1;
+//        }
+//        return re1.getHoldCount() + re2.getHoldCount() + re3.getHoldCount();
+        return numMonkeysOnRope;
     }
 }
