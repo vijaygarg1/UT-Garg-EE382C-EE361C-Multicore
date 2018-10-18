04ea669d85098b30388f3b3c60604fc4de238232
kevinmbt <kperilla@utexas.edu>
2018-10-17 20:00:05 -0500

    q6 done

--- hw-template/hw3/q6/stack/LockFreeStack.java
+++ hw-template/hw3/q6/stack/LockFreeStack.java
@@ -1,29 +1,50 @@
 package stack;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class LockFreeStack implements MyStack {
-// you are free to add members
+    private AtomicReference<Node> top;
 	
-  public LockFreeStack() {
-	  // implement your constructor here
-  }
-	
-  public boolean push(Integer value) {
-	  // implement your push method here
-	  return false;
-  }
-  
-  public Integer pop() throws EmptyStack {
-	  // implement your pop method here
-	  return null;
-  }
-  
-  protected class Node {
-	  public Integer value;
-	  public Node next;
-		    
-	  public Node(Integer x) {
-		  value = x;
-		  next = null;
-	  }
-  }
+    public LockFreeStack() {
+        top = new AtomicReference<>(null);
+    }
+
+    public boolean push(Integer value) {
+        Node n = new Node(value);
+        while (true) {
+            Node oldTop = top.get();
+            n.next = oldTop;
+            if (top.compareAndSet(oldTop, n)) {
+                return true;
+            } else {
+                Thread.yield();
+            }
+        }
+    }
+
+    public Integer pop() throws EmptyStack {
+        while (true) {
+            Node oldTop = top.get();
+            if (oldTop == null) {
+                throw new EmptyStack();
+            }
+            Integer val = oldTop.value;
+            Node newTop = oldTop.next;
+            if (top.compareAndSet(oldTop, newTop)) {
+                return val;
+            } else {
+                Thread.yield();
+            }
+        }
+    }
+
+    protected class Node {
+        public Integer value;
+        public Node next;
+
+        public Node(Integer x) {
+            value = x;
+            next = null;
+        }
+    }
 }
