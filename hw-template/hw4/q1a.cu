a004cc331c4e9fbcf54bb27f84ae84b78d18ae46
kevinmbt <kperilla@utexas.edu>
2018-10-25 21:55:14 -0500

    test

---
+++ hw-template/hw4/q1a.cu
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main () {
+  string line;
+  ifstream myfile ("example.txt");
+  if (myfile.is_open())
+  {
+    while ( getline (myfile,line) )
+    {
+      cout << line << '\n';
+    }
+    myfile.close();
+  }
+
+  else cout << "Unable to open file";
+
+  return 0;
+}
