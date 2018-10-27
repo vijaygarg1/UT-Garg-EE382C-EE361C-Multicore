24069d17632f6b55760b9be90a3a173ff7947caa
Kevin Perilla <kperilla@utexas.edu>
2018-10-27 18:08:17 -0500

    populate array method

--- hw-template/hw4/q1a.cu
+++ hw-template/hw4/q1a.cu
@@ -1,21 +1,37 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
+#include "vector"
 using namespace std;
 
-int main () {
-  string line;
-  ifstream myfile ("example.txt");
-  if (myfile.is_open())
-  {
-    while ( getline (myfile,line) )
-    {
-      cout << line << '\n';
-    }
-    myfile.close();
+int populate_array(vector<string>* arr) {
+  ifstream infile( "inp.txt" );
+  if (!infile.is_open()) {
+    cout<<"File failed to open"<<endl;
+    return 0;
   }
+  string line;
+  while (getline(infile, line))
+  {
+    istringstream ss(line);
 
-  else cout << "Unable to open file";
+    vector <string> record;
 
-  return 0;
+    while (ss)
+    {
+      string s;
+      if (!getline(ss, s, ',')) break;
+      arr->push_back( s );
+    }
+  }
+  return 1;
+}
+
+int main () {
+  vector<string> arr;
+  if (!populate_array(&arr)) {
+    return 0;
+  }
+  cout<<arr[0]<<endl;
 }
