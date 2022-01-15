b651166c15dc8951cdf91011b826f00be12fb1ef
vijaygarg1 <vkgarg@gmail.com>
2022-01-15 11:57:37 -0600

    Delete hw-template directory

--- hw-template/hw3/q6/stack/EmptyStack.java
+++
@@ -1,9 +0,0 @@
-package stack;
-
-// This is the exception throwed when try to pop an
-// element from an empty stack.
-public class EmptyStack extends java.lang.Exception {
-    public EmptyStack() {
-        super ();
-    }
-}
